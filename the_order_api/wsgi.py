43aba57d3618777209bf25a66603c2f5b2594266
Gianluca Casu <gianlucacasu@MacBook-Air-de-Gianluca.local>
2024-04-08 19:56:57 -0500

    almost done

--- the_order_api/wsgi.py
+++ the_order_api/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
+# Configurar la variable de entorno para los ajustes de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_order_api.settings')
 
 application = get_wsgi_application()
+
+# Añadir WhiteNoise al objeto aplicación WSGI
+application = WhiteNoise(application, root=os.path.join(os.environ.get('BASE_DIR', ''), 'staticfiles'))
+
+# Opcional: si tienes otros directorios estáticos puedes agregarlos así:
+# application.add_files('/path/to/other/static/files', prefix='more-files/')
+
