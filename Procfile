22bd642347e00fcce952db0172144cda13bcb6f0
Gianluca Casu <gianlucacasu@MacBook-Air-de-Gianluca.local>
2024-04-08 19:21:39 -0500

    Commit before deploy on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn the-order.wsgi --log-file -
--- the_order_api/settings.py
+++ the_order_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)9$tva50x-*5#wi6k%r!sre)j6&(+zsbxa9vk7rnx)tvwpm8%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tuapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
